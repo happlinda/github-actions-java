9a4b3256bb892f0be693a68a43592169acb2838d
happlinda <33649207+happlinda@users.noreply.github.com>
2023-04-05 02:15:23 -0700

    Delete main.js

--- .github/workflows/actions/main.js
+++
@@ -1,4 +0,0 @@
-let body = process.env['INPUT_PRBODY']; 
-let pattern = /#\d+/;
-let issueNumber = body.match(pattern)[0].replace('#', '');
-console.log(`::set-output name=issueNumber::${issueNumber}`);
