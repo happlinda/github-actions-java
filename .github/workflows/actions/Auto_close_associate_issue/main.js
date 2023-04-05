623c45a0e3cb1a1d2eece9b15299d1ab939643ab
happlinda <33649207+happlinda@users.noreply.github.com>
2023-04-05 02:14:49 -0700

    Create main.js

---
+++ .github/workflows/actions/Auto_close_associate_issue/main.js
@@ -0,0 +1,4 @@
+let body = process.env['INPUT_PRBODY']; 
+let pattern = /#\d+/;
+let issueNumber = body.match(pattern)[0].replace('#', '');
+console.log(`::set-output name=issueNumber::${issueNumber}`);
